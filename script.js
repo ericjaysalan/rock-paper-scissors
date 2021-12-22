f886c3a4a451068ec42ac39c032e54ee40441e8f
Eric Jay Salan <ericjaysalan@yahoo.com>
2021-12-22 12:07:42 +0800

    Able to play 1 round

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
-console.log("Hello World!");
+// create a function that evaluates player and computer choices
+// return a string that declares the winner
+const choices = ["rock", "paper", "scissors"];
+const playerPick = prompt("Rock, Paper, Scissors: ").toLowerCase();
+
+const getRandomPick = () => choices[Math.floor(Math.random() * choices.length)];
+const computerPick = getRandomPick();
+
+const playRound = (playerPick, computerPick) => {
+  let result;
+  const playersChoices = `\nYou: ${playerPick}\nComputer: ${computerPick}`;
+  const winMessage = `You Win!${playersChoices}`;
+  const loseMessage = `You Lose!${playersChoices}`;
+  const tieMessage = `Tie!${playersChoices}`;
+  
+  // Evaluate order is: tie, win lose
+  if (playerPick === "rock") {
+    if (computerPick === "rock") {
+      result = tieMessage;
+    } else if (computerPick === "scissors") {
+      result = winMessage;
+    } else {
+      result = loseMessage;
+    }
+  } else if (playerPick === "paper") {
+    if (computerPick === "paper") {
+      result = tieMessage;
+    } else if (computerPick === "rock") {
+      result = winMessage;
+    } else {
+      result = loseMessage;
+    }
+  } else {
+    if (computerPick === "scissors") {
+      result = tieMessage;
+    } else if (computerPick === "paper") {
+      result = winMessage;
+    } else {
+      result = loseMessage
+    }
+  }
+
+  return result;
+}
+
+console.log(playRound(playerPick, computerPick));
